36c2a6859be84b965c1bfbd53b5a80e64a04007a
Ilham Imaduddin <ilhamadun@gmail.com>
2017-10-12 23:11:27 +0700

    Capture image from a camera app
    - Send image capture intent
    - Save the original image
    - Receive image path

--- app/src/main/java/id/ac/ugm/chiliquality/MainActivity.kt
+++ app/src/main/java/id/ac/ugm/chiliquality/MainActivity.kt
@@ -1,23 +1,36 @@
 package id.ac.ugm.chiliquality
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val REQUEST_IMAGE_CAPTURE = 1
+    }
+
+    private var currentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener { _ ->
+            dispatchTakePictureIntent()
         }
     }
 
@@ -35,5 +48,46 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    /**
+     * Take picture with other camera application.
+     */
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            var photoFile: File? = null
+
+            try {
+                photoFile = createImageFile()
+            } catch (ex: IOException) {
+                // Error occurred while creating the File
+            }
+
+            if (photoFile != null) {
+                val photoUri = FileProvider.getUriForFile(
+                        this, "id.ac.ugm.chiliquality.file_provider", photoFile)
+
+                Log.i(TAG, "Saving image to: " + currentPhotoPath)
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    /**
+     * Create image file to store the image from camera.
+     */
+    private fun createImageFile(): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "CHILI_" + timestamp
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(imageFileName, ".jpg", storageDir)
+
+        currentPhotoPath = image.absolutePath
+
+        return image
     }
 }
